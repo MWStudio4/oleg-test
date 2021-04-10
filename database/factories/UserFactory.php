4e7fddea432acda4538a0ccf3e477a74596a70fb
MWStudio4 <mwstudio4@gmail.com>
2021-04-10 12:33:30 +0300

    add Post model

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,9 +22,10 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $id = $this->faker->unique()->randomDigit;
         return [
-            'name' => $this->faker->name,
-            'email' => $this->faker->unique()->safeEmail,
+            'name' => 'User '.$id,
+            'email' => 'user'.$id.'@1.cc',
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
